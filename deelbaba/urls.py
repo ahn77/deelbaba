93bd0dd3560400b147b80f028d69ee21cfe31983
ahn77 <manuahnas7@gmail.com>
2024-08-18 13:33:47 +0530

    asdasd

--- deelbaba/urls.py
+++ deelbaba/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('',include('official.urls',namespace='official'))
     
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
