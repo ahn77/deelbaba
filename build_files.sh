65850b8a2e0bc1def2edb6c29d6bbf25149a8d37
ahn77 <manuahnas7@gmail.com>
2024-08-18 06:26:02 +0530

    y

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,2 @@
-# build_files.sh
-python3.12 -m pip install -r requirements.txt
-
-# make migrations
-python3.12 manage.py migrate 
-python3.12 manage.py collectstatic
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
