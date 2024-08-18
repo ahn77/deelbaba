65ec85561aa02cecee53501d42a37f5ce2e56fba
ahn77 <manuahnas7@gmail.com>
2024-08-18 10:53:12 +0530

    12

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+# Ensure pip is installed and up-to-date
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies
+python3.9 -m pip install -r requirements.txt
+
+# Run migrations and collect static files
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
+
