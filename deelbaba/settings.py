bb3309b70b5a768bb8fe1fd5ee1f0ed8d8d15e7f
ahn77 <manuahnas7@gmail.com>
2024-08-18 07:35:13 +0530

    ugfghjkl

--- deelbaba/settings.py
+++ deelbaba/settings.py
@@ -135,8 +135,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_FILE_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static','assets')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
