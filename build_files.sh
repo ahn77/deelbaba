a1a667e5c8e9434e1e9a151df1c20c2e2b6d48d7
ahn77 <manuahnas7@gmail.com>
2024-08-18 07:39:54 +0530

    sadafaf

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,2 @@
-
-# Ensure pip is installed
-python3.9 -m ensurepip
-python3.9 -m pip install --upgrade pip
-
-# Install dependencies
-python3.9 -m pip install -r requirements.txt
-
-# Run migrations and collect static files
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
--- deelbaba/settings.py
+++ deelbaba/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -135,8 +135,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_FILE_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
