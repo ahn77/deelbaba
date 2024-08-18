2cb23f53089b749ee726ad93d57d4ff9591685ce
ahn77 <manuahnas7@gmail.com>
2024-08-18 06:36:22 +0530

    qwertyuijhgvfdswertyhn

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
+
+# Ensure pip is installed
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run migrations and collect static files
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput
