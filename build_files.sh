141ddf22100bce9cf9e39ce4e489efc66eda673f
ahn77 <manuahnas7@gmail.com>
2024-08-18 12:00:40 +0530

    SDSAD

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,12 @@
-python3.12 -m ensurepip
-python3.12 -m pip install --upgrade pip
+#!/bin/bash
+
+# Ensure pip is installed and up-to-date
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
 
 # Install dependencies
-python3.12 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run migrations and collect static files
-python3.12 manage.py migrate
-python3.12 manage.py collectstatic --noinput --clear
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
