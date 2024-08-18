0e648bf5b32eb801f90796d7d20e114dedeb1982
ahn77 <manuahnas7@gmail.com>
2024-08-18 07:50:03 +0530

    A4

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 
-# Ensure pip is installed
-python3.10 -m ensurepip
-python3.10 -m pip install --upgrade pip
+# Ensure pip is installed and up-to-date
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
 
 # Install dependencies
-python3.10 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run migrations and collect static files
-python3.10 manage.py migrate
-python3.10 manage.py collectstatic
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic 
