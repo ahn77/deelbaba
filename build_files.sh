7e0bd1669d10ce7dc9c3276de645da82fd979c64
ahn77 <manuahnas7@gmail.com>
2024-08-18 10:39:41 +0530

    1

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,4 @@
-#!/bin/bash
-
-# Ensure pip is installed and up-to-date
-python3.9 -m ensurepip
-python3.9 -m pip install --upgrade pip
-
-# Install dependencies
-python3.9 -m pip install -r requirements.txt
-
-# Run migrations and collect static files
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic 
-
-sudo chmod 777 static
+ echo "BUILD START"
+ python3.9 -m pip install -r requirements.txt
+ python3.9 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
--- deelbaba/settings.py
+++ deelbaba/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'deelbaba.wsgi.application'
+WSGI_APPLICATION = 'deelbaba.wsgi.app'
 
 
 # Database
@@ -134,9 +134,8 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
-STATIC_FILE_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
