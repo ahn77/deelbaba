0e5b3c5af86ee0832a8de685782ff743e61b98a2
ahn77 <manuahnas7@gmail.com>
2024-08-18 12:02:07 +0530

    ASDASD

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,9 @@
-python3.12 -m ensurepip
-python3.12 -m pip install --upgrade pip
+python3.9 -m ensurepip
+python3.9 -m pip install --upgrade pip
 
 # Install dependencies
-python3.12 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Run migrations and collect static files
-python3.12 manage.py migrate
-python3.12 manage.py collectstatic --noinput --clear
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic --noinput --clear
